b48f3a01d061d44996911a10ec0d5b8f1e9ad47d
U.Olga <u.olga@gmail.com>
2013-09-19 00:15:08 +0600

    Сделала первую лабу

---
+++ Лаб1/Lab1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Lab1.Models;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // Post: /Home/
+        [HttpPost]
+        public ActionResult Save(DataModel model)
+        {
+            return View("Info",model);
+        }
+         
+    }
+}
---
+++ Лаб1/Lab1/Models/DataModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class DataModel
+    {
+        [Required]
+        [DisplayName("Фамилия")]
+        public string SurName { get; set; }
+
+        [Required]
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [DisplayName("Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+
+        [Required]
+        [DisplayName("Пол")]
+        public Sex Sex { get; set; }
+    }
+
+    public enum Sex
+    {
+        Жен = 0,
+        Муж = 1
+    }
+}
